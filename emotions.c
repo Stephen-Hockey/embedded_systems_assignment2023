3753b0efe0d8c900354f7d2f1f45cbb8f6aff66b
Xinwei Wang <xwa133@uclive.ac.nz>
2023-10-18 15:53:19 +1300

    Added bitmap emotions for use later

---
+++ emotions.c
@@ -0,0 +1,41 @@
+#include "system.h"
+
+/** happy  */
+static const uint8_t bitmapHappy[] =
+{
+    0b1100011,
+    0b0000000,
+    0b0000000,
+    0b0100010,
+    0b0011100
+};
+
+/** unhappy  */
+static const uint8_t bitmapUnhappy[] =
+{
+    0b0100010,
+    0b1000001,
+    0b0000000,
+    0b0011100,
+    0b0100010
+};
+
+/** angry */
+static const uint8_t bitmapAngry[] =
+{
+    0b1100011,
+    0b0000000,
+    0b0000000,
+    0b0011100,
+    0b0100010
+};
+
+/** furious */
+static const uint8_t bitmapFurious[] =
+{
+    0b1000001,
+    0b0100010,
+    0b0000000,
+    0b0011100,
+    0b0100010
+};
