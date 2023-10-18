d707c344a29e7cd9479310cca1d394cdfde6b234
Xinwei Wang <xwa133@uclive.ac.nz>
2023-10-18 19:03:08 +1300

    Got ball to show up on batter screen correctly

--- game.c
+++ game.c
@@ -10,14 +10,15 @@
 #include "display.h"
 #include "tinygl.h"
 #include "font.h"
-
+#include "../fonts/font5x7_1.h"
 #include "ir_uart.h"
 
 #define PACER_RATE 200
 #define NAVSWITCH_CHECK_RATE 200
 #define DRAW_RATE 200
-#define POWER_BAR_UPDATE_RATE 2
+#define POWER_BAR_UPDATE_RATE 5
 #define IR_CHECK_RATE 200
+
 
 typedef enum {
     GAME_LAUNCHED,          /* When a game is first started, roles need to be chosen */
@@ -51,16 +52,32 @@ typedef struct {
     const uint8_t row;
 } batter_t;
 
+typedef struct {
+    uint8_t col;
+    uint8_t row;
+    uint8_t tick_rate;
+} thrown_ball_t;
 
-static game_state_t game_state = PITCHER_CHOOSE; // Should be set to GAME_LAUNCHED initally
+
+static game_state_t game_state = GAME_LAUNCHED; // Should be set to GAME_LAUNCHED initally
 
 static pitcher_t pitcher = {.col = 2, .row = 3}; 
 static power_bar_t power_bar = {.power = 0, .increment_value = 1};
 static batter_t batter = {.col = 2, .row = 6};
+static thrown_ball_t thrown_ball;
+static uint16_t homeruns;
+static uint8_t strikes;
+
+
 
 // IR values
 static uint8_t chosen_pitch_col;
 static uint8_t chosen_pitch_power;
+static const char LAUNCH_MSG[] = "BATTER UP?"; //TODO
+
+bool ball_hit_p() {
+    return 1;
+}
 
 void navswitch_north_pushed()
 {
@@ -72,6 +89,12 @@ void navswitch_north_pushed()
     case PITCHER_TIMING:
         break;    
     case PITCHER_BALL_THROWN:
+        // the swing
+        if (ball_hit_p) {
+            homeruns++;
+        } else {
+            strikes++;
+        }
         break;    
     case BATTER_IDLE:
         break;    
@@ -114,12 +137,15 @@ void navswitch_east_pushed()
     case PITCHER_TIMING:
         break;    
     case PITCHER_BALL_THROWN:
+    
         break;    
     case BATTER_IDLE:
         if (batter.col < LEDMAT_COLS_NUM - 2)
             batter.col++;
         break;    
     case BATTER_BALL_THROWN:
+        if (batter.col < LEDMAT_COLS_NUM - 2)
+            batter.col++;
         break;        
     default:
         break;
@@ -138,12 +164,15 @@ void navswitch_west_pushed()
     case PITCHER_TIMING:
         break;    
     case PITCHER_BALL_THROWN:
+        
         break;    
     case BATTER_IDLE:
         if (batter.col > 0)
             batter.col--;
         break;    
     case BATTER_BALL_THROWN:
+        if (batter.col > 0)
+            batter.col--;
         break;        
     default:
         break;
@@ -155,6 +184,8 @@ void navswitch_push_pushed()
     uint8_t ball_packet; 
     switch (game_state) {
     case GAME_LAUNCHED:
+        ir_uart_putc('a'); // the character is irrelevant but can use for checking
+        game_state = BATTER_IDLE;        
         break;
     case PITCHER_CHOOSE:
         chosen_pitch_col = pitcher.col;
@@ -208,6 +239,11 @@ void check_ir()
     uint8_t ball_packet;
     switch (game_state) {
     case GAME_LAUNCHED:
+        if (ir_uart_read_ready_p()) {
+            if (ir_uart_getc() == 'a') {
+                game_state = PITCHER_CHOOSE;
+            }            
+        }
         break;
     case PITCHER_CHOOSE:
         break;    
@@ -216,15 +252,38 @@ void check_ir()
     case PITCHER_BALL_THROWN:
         break;    
     case BATTER_IDLE:
-
         if (ir_uart_read_ready_p()) {
             ball_packet = ir_uart_getc();
             if (ball_packet_valid_p) {
                 chosen_pitch_col = ball_packet >> 3;
                 chosen_pitch_power = ball_packet & 0b111;
+                thrown_ball.col = chosen_pitch_col;
+                thrown_ball.row = 0;
+                switch (chosen_pitch_power)
+                {
+                case 0:
+                    thrown_ball.tick_rate = 2;
+                    break; 
+                case 1:
+                    thrown_ball.tick_rate = 4;
+                    break;
+                case 2:
+                case 3:
+                    thrown_ball.tick_rate = 8;
+                    break;    
+                case 4:
+                case 5:
+                    thrown_ball.tick_rate = 12;
+                    break;    
+                case 6:
+                    thrown_ball.tick_rate = 20;
+                    break;        
+                default:
+                    break;
+                }
+                game_state = BATTER_BALL_THROWN;
             }            
         }
-
         break;    
     case BATTER_BALL_THROWN:
         break;        
@@ -244,14 +303,24 @@ void update_power_bar()
     power_bar.power += power_bar.increment_value;
 }
 
+void update_thrown_ball()
+{
+    if (thrown_ball.row < LEDMAT_ROWS_NUM - 1) {
+        thrown_ball.row++;
+    } else {
+        
+    }
+        
+}
+
 void draw_all()
 {   
-    // declare here because not allowed to inside of case blocks
     tinygl_point_t pitcher_point = {pitcher.col, pitcher.row};
     tinygl_point_t power_bar_left_point = {.x = 0};
     tinygl_point_t power_bar_right_point = {.x = LEDMAT_COLS_NUM - 1};
     tinygl_point_t batter_left_point = {batter.col, batter.row};
     tinygl_point_t batter_right_point = {batter.col + 1, batter.row};
+    tinygl_point_t thrown_ball_point = {thrown_ball.col, thrown_ball.row};
 
     switch (game_state) {
     case GAME_LAUNCHED:
@@ -269,10 +338,12 @@ void draw_all()
     case PITCHER_BALL_THROWN:
         break;    
     case BATTER_IDLE:
-        // tinygl_draw_line(batter_left_point, batter_right_point, 1);
-        display_pixel_set(chosen_pitch_col, chosen_pitch_power, 1);
+        tinygl_draw_line(batter_left_point, batter_right_point, 1);
         break;    
     case BATTER_BALL_THROWN:
+        tinygl_draw_point(thrown_ball_point, 1);
+        tinygl_draw_line(batter_left_point, batter_right_point, 1);
+
         break;        
     default:
         break;
@@ -291,14 +362,12 @@ int main (void)
     led_set(LED1, 0);
     ir_uart_init();
 
-
-
     // Declare tick counters
     uint8_t navswitch_check_ticks;
     uint8_t power_bar_update_ticks;
+    uint8_t thrown_ball_ticks;
     uint8_t ir_ticks;
     uint8_t draw_ticks;
-    
 
     // Main game loop
     while (1) {
@@ -317,6 +386,14 @@ int main (void)
             if (power_bar_update_ticks >= PACER_RATE/POWER_BAR_UPDATE_RATE) {
                 update_power_bar();
                 power_bar_update_ticks = 0;
+            }
+        }
+
+        if (game_state == BATTER_BALL_THROWN) {
+            thrown_ball_ticks++;
+            if (thrown_ball_ticks >= PACER_RATE/thrown_ball.tick_rate) {
+                update_thrown_ball();
+                thrown_ball_ticks = 0;
             }
         }
 
